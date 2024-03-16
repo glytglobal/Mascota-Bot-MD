4d1a2a59ad43f8b34755bbb0526e592abb009410
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-03-16 19:25:49 -0300

    𝙈𝙖𝙨𝙘𝙤𝙩𝙖-𝘽𝙤𝙩-𝙈𝘿 🌟

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ RUN sudo dnf -y update &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
     sudo dnf clean all -y
 
-RUN git clone https://github.com/GataNina-Li/GataBot-MD
+RUN git clone https://github.com/glytglobal/Mascota-Bot-MD
 
-WORKDIR /root/GataBot-MD
+WORKDIR /root/Mascota-Bot-MD
 
-COPY ./root/GataBot-MD
+COPY ./root/Mascota-Bot-MD
 
 RUN yarn install
 
